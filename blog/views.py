c5ba979b390755e5bf562d55b984056a5d2ab811
clover <759983462@qq.com>
2018-02-28 14:08:16 +0800

    first commit

---
+++ blog/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from blog.models import BlogsPost
+
+def blog_index(request):
+	blog_list = BlogsPost.objects.all()
+	return render(request, 'index.html', {'blog_list':blog_list})
+	pass
+
+# Create your views here.
